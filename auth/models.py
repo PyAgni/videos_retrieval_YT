3344032168bf3c76cf72180e064e1d12fb058ceb
Agni <bhattacharyya.agni@gmail.com>
2022-08-21 04:50:34 +0530

    Removed auth app

--- auth/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class APIKey(models.Model):
-    api_key = models.CharField(max_length=50, unique=True)
-    has_expired = models.BooleanField(default=False, null=False)
