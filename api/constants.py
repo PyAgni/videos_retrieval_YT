c0b5ca730fd35ef51f2783e2f6da2918a13444fe
Agni <bhattacharyya.agni@gmail.com>
2022-08-21 02:04:00 +0530

    Added Video model

---
+++ api/constants.py
@@ -0,0 +1,2 @@
+YT_API_URL = "https://www.googleapis.com/youtube/v3/search"
+YT_BASE_URL = "https://www.youtube.com/watch?v="
---
+++ api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from api.constants import YT_BASE_URL
+
+
+class Video(models.Model):
+    youtube_video_id = models.CharField(max_length=11, unique=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=5000, blank=True, null=True)
+    published_at = models.DateTimeField()
+    thumbnail_url = models.URLField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Title:{str(self.title)}\nURL:{YT_BASE_URL + str(self.youtube_video_id)}'
