7de71ca2b0bc843b1068a492a2385e5fa4da6e93
Agni <bhattacharyya.agni@gmail.com>
2022-08-21 02:38:06 +0530

    Added APIKey model in auth app

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class APIKey(models.Model):
+    api_key = models.CharField(max_length=50, unique=True)
+    has_expired = models.BooleanField(default=False, null=False)
